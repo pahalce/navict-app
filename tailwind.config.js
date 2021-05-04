8d0e6a6c89bf30d63390f3e30ad7a858bd5630e9
pahalce <peaceee.programming@gmail.com>
2021-05-05 00:48:37 +0900

    imported fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,44 +9,39 @@ module.exports = {
       $accent2: '#E97C4C',
       $accent3: '#F2C94C',
       $accent4: '#73B1A8',
-      $primary: '#2C2738',
-      $primary2: '#2C2738',
-      $muted: '#7C9CBF',
-      $shade: '#F2F2F2',
-      $tint: '#FAFDFF'
+      $primary: '#3F3F3F',
+      $primary2: '#979797',
+      $indigo: '#7C9CBF',
+      $tint: '#FAFDFF',
+      $shade1: '#C0C0C0',
+      $shade2: '#F2F2F2',
+      $shade3: '#FAFAFA'
     },
     fontFamily: {
       noto: 'Noto Sans JP',
       josefin: 'Josefin Sans',
       roboto: 'Roboto',
-      body: ['Noto Sans JP', 'Josefin Sans', 'Roboto']
+      body: ['Noto Sans JP', 'Josefin Sans', 'Roboto', 'sans-serif']
     },
-    extend: {}
+    extend: {
+      fontSize: {
+        '3.5xl': '2rem'
+      }
+    }
   },
   variants: {
     extend: {}
   },
   plugins: [
     plugin(function ({ addUtilities, theme }) {
-      const newUtilities = {
-        // '.text-$t1': {
-        //   fontFamily: theme('fontFamily.noto'),
-        //   fontWeight: 'bold',
-        //   fontSize: theme('fontSize.4xl')
-        // },
-        // '.text-$t2': {
-        //   fontFamily: theme('fontFamily.noto'),
-        //   fontWeight: 'bold',
-        //   fontSize: theme('fontSize.5xl')
-        // }
-      }
+      const newUtilities = {}
       const fontSystems = {
         types: [
           { prefix: '.text-$t', family: theme('fontFamily.noto') },
-          { prefix: '.text-$TEXT', family: theme('fontFamily.josefin') }
+          { prefix: '.text-$T', family: theme('fontFamily.josefin') }
         ],
-        sizes: ['64', '36', '24', '20', '18', '16', '14'],
-        weight: 'bold',
+        sizes: ['64px', '36px', '24px', '20px', '18px', '16px', '14px'],
+        weights: ['700', '700', '700', '700', '500', '400', '400'],
         letterSpacings: ['-2%', '3%', '3%', '3%', '3%', '0%', '-2%']
       }
       fontSystems.types.forEach((type) => {
@@ -54,7 +49,7 @@ module.exports = {
           newUtilities[type.prefix + i.toString()] = {
             fontFamily: type.family,
             fontSize: fontSystems.sizes[i],
-            fontWeight: fontSystems.weight,
+            fontWeight: fontSystems.weights[i],
             letterSpacing: fontSystems.letterSpacings[i]
           }
         }
