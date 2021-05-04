52b3dbd1ce1a679e6d9fc9e0659af264f01c09c3
pahalce <peaceee.programming@gmail.com>
2021-05-04 16:21:14 +0900

    styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      b_white: '#FFFFFF',
+      b_lightblue: '#E7F5FB',
+      b_greyblue: '#F8FAFF',
+      b_black: '#3F3F3F',
+      b_blue: '#76C3DB',
+      b_red: '#E97C4C',
+      b_yellow: '#F5B76B',
+      b_green: '#73B1A8'
+    },
+    fontFamily: {
+      noto: 'Noto Sans JP',
+      josefin: 'Josefin Sans',
+      roboto: 'Roboto',
+      body: ['Noto Sans JP', 'Josefin Sans', 'Roboto']
+    },
     extend: {}
   },
   variants: {
