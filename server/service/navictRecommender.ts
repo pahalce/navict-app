76c45892168c099be41a35894bde003917ff3a36
渡辺豪志 <tuyosi.628@gmail.com>
2021-05-04 19:36:58 +0900

    refactor navictRecommender service

--- server/service/navictRecommender.ts
+++ server/service/navictRecommender.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { sortSteps } from '$/service/roadmaps'
+import { Roadmap, Step } from '$prisma/client'
 
 const prisma = new PrismaClient()
 
@@ -18,25 +18,49 @@ export const getAllRoadmapsLibraryIds = async (): Promise<
    * 2 → 3 → 4 → 5 → 6 → 7 → 8 → 1(Goal) の順番の場合、
    * 返り値は [[0,0,0,2], [0,0,2,3], [0,2,3,4], [2,3,4,5], [3,4,5,6], [4,5,6,7], [5,6,7,8], [6,7,8,1]]
    */
-  let ids: [number, number, number, number][] = []
+  const ids: [number, number, number, number][] = []
   const roadmaps = await prisma.roadmap.findMany({
     include: {
       steps: true
     }
   })
-  await roadmaps.map(async (roadmap) => {
-    const steps = (await sortSteps(roadmap)).steps
-    await [...Array(steps.length)].map((_, i) => {
-      ids = [
-        ...ids,
-        [
-          steps[i - 3]?.libraryId || 0,
-          steps[i - 2]?.libraryId || 0,
-          steps[i - 1]?.libraryId || 0,
-          steps[i - 0]?.libraryId || 0
-        ]
-      ]
-    })
+  roadmaps.map((roadmap) => {
+    const steps = sortSteps(roadmap).steps
+    for (let i = 0; i < steps.length; i++) {
+      ids.push([
+        steps[i - 3]?.libraryId || 0,
+        steps[i - 2]?.libraryId || 0,
+        steps[i - 1]?.libraryId || 0,
+        steps[i - 0]?.libraryId || 0
+      ])
+    }
   })
   return ids
+}
+
+const sortSteps = (
+  roadmap: Roadmap & { steps: Step[] }
+): Roadmap & { steps: Step[] | [] } => {
+  const sortedSteps: Step[] = []
+
+  // 1番目のStepを追加
+  const firstStep = roadmap.steps.find(
+    (step) => step.id === roadmap.firstStepId
+  )
+  if (!firstStep) return { ...roadmap, steps: [] }
+  sortedSteps.push(firstStep)
+
+  // 2番目以降のStepを追加
+  let nextStepId: Step['nextStepId'] = null
+  for (let i = 0; i < roadmap.steps.length - 1; i++) {
+    const nextStep = roadmap.steps.find(
+      (step) => step.id === sortedSteps[-1]?.nextStepId
+    )
+    if (!nextStep) break
+    sortedSteps.push(nextStep)
+    nextStepId = nextStep?.nextStepId || null
+    if (!nextStepId) break
+  }
+
+  return { ...roadmap, steps: sortedSteps }
 }
--- server/service/roadmaps.ts
+++ server/service/roadmaps.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import type { Prisma, Roadmap, Step } from '$prisma/client'
+import type { Prisma, Roadmap } from '$prisma/client'
 
 const prisma = new PrismaClient()
 
@@ -72,28 +72,4 @@ export const toggleIsDone = async (id: Roadmap['id']) => {
     where: { id },
     data: { isDone: !roadmap?.isDone }
   })
-}
-
-export const sortSteps = async (
-  roadmap: Roadmap & { steps: Step[] }
-): Promise<Roadmap & { steps: Step[] | [] }> => {
-  const sortedSteps: Step[] = []
-
-  // 1番目のStepを追加
-  const firstStep = roadmap.steps.find(
-    (step) => step.id === roadmap.firstStepId
-  )
-  if (!firstStep) return { ...roadmap, steps: [] }
-  sortedSteps.push(firstStep)
-
-  // 2番目以降のStepを追加
-  for (let i = 0; i < roadmap.steps.length - 1; i++) {
-    const nextStep = roadmap.steps.find(
-      (step) => step.id === sortedSteps[-1]?.nextStepId
-    )
-    if (!nextStep) continue
-    sortedSteps.push(nextStep)
-  }
-
-  return { ...roadmap, steps: sortedSteps }
 }
