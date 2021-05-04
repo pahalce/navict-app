2d6634cd11d2c20feb93f9cec590ab77cbeb8c15
渡辺豪志 <tuyosi.628@gmail.com>
2021-05-04 19:04:19 +0900

    fix comment

--- server/api/navictRecommender/roadmaps/libraryIds/index.ts
+++ server/api/navictRecommender/roadmaps/libraryIds/index.ts
@@ -2,7 +2,7 @@ import { Step } from '$prisma/client'
 
 type LibraryId = Step['libraryId']
 export type Methods = {
-  // get all roadmaps libraryIds
+  // get all roadmaps' libraryIds
   get: {
     resBody: [LibraryId, LibraryId, LibraryId, LibraryId][]
   }
