0a493e75ca0294fec60c8e54825f516b35770630
渡辺豪志 <tuyosi.628@gmail.com>
2021-05-06 14:04:38 +0900

    recreate POST /libraries/recommended api

--- server/api/libraries/recommended/index.ts
+++ server/api/libraries/recommended/index.ts
@@ -1,10 +1,14 @@
 import type { Library } from '$prisma/client'
 
+type LibraryId = Library['id']
 type RecommendedLibrary = Library & { score: number }
 export type Methods = {
-  // get recommended libraries
-  get: {
-    reqBody: Library['id'][]
+  /**
+   * get recommended libraries
+   * POST /libraries/recommended
+   */
+  post: {
+    reqBody: LibraryId[]
     resBody: RecommendedLibrary[]
   }
 }
--- server/service/libraries.ts
+++ server/service/libraries.ts
@@ -6,14 +6,19 @@ const prisma = new PrismaClient()
 export const createLibrary = (title: Library['title'], link: Library['link']) =>
   prisma.library.create({ data: { title, link } })
 
-// FIXME: navict-recommenderに問い合わせる様に変更する。
-export const getRecommendedLibraries = async (ids: number[]) =>
-  (await prisma.library.findMany()).slice(0, 3).map((library) => {
+/**
+ * FIXME:
+ * navict-recommenderに問い合わせる様に変更する。
+ * goalの仕様固まったら実装修正する。
+ */
+export const getRecommendedLibraries = async (ids: Library['id'][]) => {
+  return (await prisma.library.findMany()).slice(0, 3).map((library) => {
     return {
       ...library,
       score: 50
     }
   })
+}
 
 export const searchLibrariesByTitle = (title: string) =>
   prisma.library.findMany({
