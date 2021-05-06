54772f5eef0bf3232fecb7c8a927ec9f72793fae
pahalce <peaceee.programming@gmail.com>
2021-05-06 15:39:45 +0900

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div className="absolute bottom-0 bg-$indigo w-full h-60"></div>
+  return <div className="bg-$indigo w-full h-60"></div>
 }
 
 export default Footer
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,26 @@ import useAspidaSWR from '@aspida/swr'
 import RoadmapCard from '$components/list/RoadmapCard'
 import { apiClient } from '~/utils/apiClient'
 
-const Home = () => {
+const PopularRoadmaps = () => {
   const { data: roadmaps, error } = useAspidaSWR(apiClient.roadmaps.popular)
   if (error) return <div>failed to load</div>
   return (
-    <div>
+    <div className="px-48 mx-auto">
+      <h2 className="text-$primary text-$t1 text-center pt-4">
+        人気のロードマップ
+      </h2>
+      {roadmaps?.map((roadmap) => (
+        <RoadmapCard key={roadmap.id} roadmap={roadmap} />
+      ))}
+    </div>
+  )
+}
+
+const Home = () => {
+  return (
+    <div className="bg-$tint w-full pb-36">
       <h1 className="px-10">navictのロードマップでなんちゃららしよう。</h1>
-      <div className="bg-$tint w-full">
-        <div className="roadmaps px-48 mx-auto">
-          <h2 className="text-$primary text-$t1 text-center pt-4">
-            人気のロードマップ
-          </h2>
-          {roadmaps?.map((roadmap) => (
-            <RoadmapCard key={roadmap.id} roadmap={roadmap} />
-          ))}
-        </div>
-      </div>
+      <PopularRoadmaps />
     </div>
   )
 }
