ccb141157358cec26e540b96e638064ed15d5d14
渡辺豪志 <tuyosi.628@gmail.com>
2021-05-08 01:07:53 +0900

    recreate likes api

--- server/api/likes/controller.ts
+++ server/api/likes/controller.ts
@@ -1,9 +1,9 @@
 import { defineController } from './$relay'
-import { createLike } from '$/service/likes'
+import { toggleLike } from '$/service/likes'
 
 export default defineController(() => ({
   post: async ({ body }) => ({
-    status: 201,
-    body: await createLike(body.userId, body.roadmapId)
+    status: 204,
+    body: await toggleLike(body.userId, body.roadmapId)
   })
 }))
