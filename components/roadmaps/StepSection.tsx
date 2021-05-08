d5e05faf02d90672dce43b4fcc4ba9972d69e4dd
pahalce <peaceee.programming@gmail.com>
2021-05-09 01:11:05 +0900

    create new roadmap progress

--- components/roadmaps/StepSection.tsx
+++ components/roadmaps/StepSection.tsx
@@ -1,37 +1,81 @@
 import Image from 'next/image'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+import { StepInfo } from '$/types/index'
+import ButtonSmall from '../button/ButtonSmall'
+import StepCard from '../list/StepCard'
 
-const StepSection = () => {
+type Props = {
+  steps: StepInfo[]
+  onAddStep: (step: StepInfo) => void
+  onDeleteStep: (step: StepInfo) => void
+  onAddLibrary: (library: StepInfo['library']) => void
+}
+
+const StepSection = ({ steps, onAddStep, onDeleteStep }: Props) => {
   const [isFormShown, setIsFormShown] = useState(false)
+  const titleRef = useRef<HTMLInputElement>(null)
+  const urlRef = useRef<HTMLInputElement>(null)
 
   const toggleShowForm = () => {
     setIsFormShown(!isFormShown)
+  }
+  const handleAddStep = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    const title = titleRef?.current?.value
+    const url = !urlRef?.current?.value
+    if (title || url) {
+      return
+    }
+    // const step: StepInfo = {
+    //   isDone: false,
+    //   library,
+    // }
+    // onAddStep(step)
+  }
+  const handleDeleteStep = (step: StepInfo) => {
+    onDeleteStep(step)
   }
 
   return (
     <div className="flex justify-center bg-$tint py-16 text-$primary">
       <div className="bg-$white text-$t2 text-center rounded-3xl text-$primary shadow-$rich w-full max-w-2xl py-14 px-10">
+        {steps.map((step) => (
+          <StepCard
+            href={step.library.link || ''}
+            key={step.id}
+            src={step.library.img || '/pencil.svg'}
+            memo={step.memo || ''}
+            title={step.library.title}
+            canDelete
+            onDeleteClick={() => handleDeleteStep(step)}
+          />
+        ))}
         <div onClick={toggleShowForm} className="cursor-pointer">
-          最初のステップを決めてみよう{' '}
+          {steps.length === 0
+            ? '最初のステップを決めてみよう'
+            : '次のステップを決めてみよう'}{' '}
           {!isFormShown && (
             <Image src="/plus.svg" width="20" height="20" layout="fixed" />
           )}
         </div>
         {isFormShown && (
           <div className="mt-10">
-            <form>
+            <div>
               <input
                 className="bg-$shade3 rounded-md text-$t4 px-4 py-2 w-full mb-4"
                 type="text"
                 placeholder="本の名前やサイトの名前を入力してみよう"
+                ref={titleRef}
               />
               <input
                 className="bg-$shade3 rounded-md text-$t4 px-4 py-2 w-full"
                 type="text"
                 placeholder="https://navict-app.vercel.app/"
+                ref={urlRef}
               />
-            </form>
+            </div>
             <div className="py-10">他の人は次にこのステップをやっています</div>
+            <ButtonSmall onClick={handleAddStep} text="追加" />
           </div>
         )}
       </div>
--- pages/roadmaps/new.tsx
+++ pages/roadmaps/new.tsx
@@ -1,16 +1,39 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import ButtonSmall from '~/components/button/ButtonSmall'
 import RoadmapBasicInfo from '~/components/roadmaps/RoadmapBasicInfo'
 import SetGoal from '~/components/roadmaps/SetGoal'
 import StepSection from '~/components/roadmaps/StepSection'
 import { apiClient } from '~/utils/apiClient'
 import { Tag } from '$prisma/client'
-import { RoadmapInfo } from '$/types/index'
+import type { RoadmapInfo, StepInfo } from '$/types/index'
+import type { RoadmapCreateReqBody } from '$/types/index'
+import { useRouter } from 'next/router'
+import { useAuth } from '~/contexts/AuthContext'
 
 const createRoadmapsPage = () => {
+  const [title, setTitle] = useState<RoadmapCreateReqBody['title']>('')
+  const [description, setDescription] = useState<
+    RoadmapCreateReqBody['description']
+  >(null)
   const [tags, setTags] = useState([] as Tag[])
   const [selectedTags, setSelectedTags] = useState<Tag[]>()
   const [goal, setGoal] = useState<RoadmapInfo['goal']>(null)
+  const [steps, setSteps] = useState<StepInfo[]>([] as StepInfo[])
+  const [libray, setLibray] = useState<StepInfo['library']>()
+  const router = useRouter()
+  const auth = useAuth()
+  if (!auth?.user) {
+    console.log('you have to be logged in')
+    router.push('/')
+  }
+  const handleTitleChange = (title: RoadmapCreateReqBody['title']) => {
+    setTitle(title)
+  }
+  const handleDescriptionChange = (
+    description: RoadmapCreateReqBody['description']
+  ) => {
+    setDescription(description)
+  }
   const handleTagKeywordChange = async (keyword: string) => {
     const result = await apiClient.tags.search._name(keyword).$get()
     setTags(result)
@@ -31,22 +54,87 @@ const createRoadmapsPage = () => {
     }
     setSelectedTags(newTags)
   }
+  const handleAddLibrary = (library: StepInfo['library']) => {
+    apiClient.libraries.post({
+      body: { title: library.title, link: library.link }
+    })
+  }
+  const handleAddStep = (step: StepInfo) => {
+    let newSteps: StepInfo[]
+    if (steps) {
+      newSteps = [...steps, step]
+    } else {
+      newSteps = [step]
+    }
+    setSteps(newSteps)
+  }
+  const handleDeleteStep = (deltedStep: StepInfo) => {
+    const newSteps: StepInfo[] = steps.filter(
+      (step) => step.id !== deltedStep.id
+    )
+    setSteps(newSteps)
+  }
   const handleGoalChange = (goal: RoadmapInfo['goal']) => {
     setGoal(goal)
   }
+
+  const handleSubmitRoadmap = async (e: React.MouseEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault()
+      const userId = auth?.user?.id
+      if (typeof userId !== 'number' || !title) {
+        return
+      }
+
+      const tagNames: Pick<Tag, 'name'>[] =
+        selectedTags?.map((selectedTag) => {
+          const name = selectedTag.name
+          return { name }
+        }) || []
+      const reqBody: RoadmapCreateReqBody = {
+        userId,
+        title,
+        tags: tagNames,
+        description: description,
+        goal,
+        firstStepId: null,
+        forkedRoadmapId: null, // TODO: forkしたときに変える
+        steps
+      }
+      console.log(reqBody)
+      const resBody = await apiClient.roadmaps.$post({ body: reqBody })
+      console.log('res:', resBody)
+      // router.push(`roadmaps/edit/${resBody.id}`)
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return (
-    <div>
+    <form onSubmit={handleSubmitRoadmap}>
       <RoadmapBasicInfo
+        title={title}
+        onChangeTitle={handleTitleChange}
+        description={description}
+        onChangeDescription={handleDescriptionChange}
         tags={tags}
         selectedTags={selectedTags}
         onTagSelect={handleTagSelect}
         onCloseSelectedTag={handleCloseSelectedTag}
         onTagKeywordChange={handleTagKeywordChange}
       />
-      <StepSection />
+      <StepSection
+        steps={steps}
+        onAddStep={handleAddStep}
+        onDeleteStep={handleDeleteStep}
+        onAddLibrary={handleAddLibrary}
+      />
       <SetGoal goal={goal} onGoalChange={handleGoalChange} />
-      <ButtonSmall text="保存" className="block mx-auto mt-40 mb-20" />
-    </div>
+      <ButtonSmall
+        type="submit"
+        text="保存"
+        className="block mx-auto mt-40 mb-20"
+      />
+    </form>
   )
 }
 
