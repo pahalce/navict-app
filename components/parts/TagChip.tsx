1466799a61032907b8b394f6913eb8c8bcc99c58
渡辺豪志 <tuyosi.628@gmail.com>
2021-05-08 18:47:56 +0900

    refactor TagChip component

--- components/parts/TagChip.tsx
+++ components/parts/TagChip.tsx
@@ -2,19 +2,19 @@ import Image from 'next/image'
 
 type TagChipProps = {
   text: string
-  onClick: () => void
+  onCrossClick: () => void
 }
-const TagChip = ({ text, onClick }: TagChipProps) => {
+const TagChip = ({ text, onCrossClick }: TagChipProps) => {
   return (
     <div
       className={`flex bg-$accent1 bg-opacity-10 rounded-full max-w-max px-4 py-2`}
     >
       <p className={`text-$t4 text-$indigo mr-2`}>{text}</p>
       <Image
-        src={`/close.svg`}
+        src={`/cross.svg`}
         width={7.29}
         height={7.29}
-        onClick={onClick}
+        onClick={onCrossClick}
         className={`cursor-pointer`}
       />
     </div>
