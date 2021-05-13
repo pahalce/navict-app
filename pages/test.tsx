9fb3afccd0ad2eb9b12575d1331b3eea8a483305
pahalce <peaceee.programming@gmail.com>
2021-05-14 01:58:05 +0900

    react hook form

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,16 +1,34 @@
-import useAspidaSWR from '@aspida/swr'
-import { apiClient } from '~/utils/apiClient'
-import { useAuth } from '~/contexts/AuthContext'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+type FormData = {
+  firstName: string
+  lastName: string
+}
 
 const TestPage = () => {
-  const auth = useAuth()
-  if (!auth || !auth.user) return <div>failed to load</div>
-  const { data: user, error: userError } = useAspidaSWR(
-    apiClient.users._userId(auth.user.id)
-  )
-  if (userError || !user) return <div>failed to load</div>
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>()
+  const onSubmit = (data) => console.log(data)
+  // firstName and lastName will have correct type
+  console.log(watch('firstName'))
 
-  return <div></div>
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>First Name</label>
+        <input {...register('firstName')} />
+        <label>Last Name</label>
+        <input {...register('lastName', { required: true })} />
+        {errors.lastName && <span>This field is required</span>}
+        <input type="submit" />
+      </form>
+    </>
+  )
 }
 
 export default TestPage
