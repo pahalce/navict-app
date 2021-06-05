095a19a615b7ea66f85974a0377fd2eaf021aed6
pahalce <peaceee.programming@gmail.com>
2021-06-05 18:57:35 +0900

    import fix

--- components/modals/UpdateStepFormModal.tsx
+++ components/modals/UpdateStepFormModal.tsx
@@ -4,7 +4,7 @@ import { LibraryInfo } from '~/server/types'
 import { Library } from '$prisma/client'
 import { SelectOption } from '../parts/SelectInput'
 import UpdateStepForm from '../roadmaps/step/UpdateStepForm'
-import { StepWithLib } from '../roadmaps/Roadmap'
+import { StepWithLib } from '../roadmaps/RoadmapComp'
 
 type Props = {
   isOpen: boolean
