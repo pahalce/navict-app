29299f0a7ea712ce10fdfc5c3162027806c391c7
pahalce <peaceee.programming@gmail.com>
2021-06-09 23:07:11 +0900

    Layout props

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { SubmitHandler } from 'react-hook-form'
 import Footer from './Footer'
 import Header, { HEADER_BTN_TYPES } from './Header'
-import { RoadmapFormSchema } from './roadmaps/RoadmapForm'
 
 type Props = {
   children: React.ReactNode
   headerType?: HEADER_BTN_TYPES
-  onHeaderSaveBtnClick?: SubmitHandler<RoadmapFormSchema>
+  onHeaderSaveBtnClick?: () => void
 }
 
 const Layout = ({ children, headerType, onHeaderSaveBtnClick }: Props) => {
