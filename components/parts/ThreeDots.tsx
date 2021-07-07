1b3f04e20fec660004f915b0758a64d2c8f83f30
pahalce <peaceee.programming@gmail.com>
2021-07-07 22:03:08 +0900

    ThreeDots resize

--- components/parts/ThreeDots.tsx
+++ components/parts/ThreeDots.tsx
@@ -2,13 +2,15 @@ import Image from 'next/image'
 
 type ThreeDotsProp = {
   onClick?: () => void
+  width?: number
+  height?: number
 }
-const ThreeDots = ({ onClick }: ThreeDotsProp) => {
+const ThreeDots = ({ onClick, width = 16, height = 20 }: ThreeDotsProp) => {
   return (
     <Image
       src={`/three-dots.svg`}
-      width={16}
-      height={20}
+      width={width}
+      height={height}
       onClick={onClick}
       className={`cursor-pointer`}
     />
